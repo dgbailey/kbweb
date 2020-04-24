f6b92e8e78eb27575ef0201a1d769479a8fe9341
dgbailey <bailey.dust@gmail.com>
2020-04-24 11:01:19 -0700

    LWW currently covers edits to existing items with entity ids.  Not newly created items.

--- src/middleware/lastWriterWins.js
+++ src/middleware/lastWriterWins.js
@@ -3,14 +3,19 @@ export const lastWriterWins = (storageManager = {}) => {
 
 	return (storeAPI) => (next) => (action) => {
 		//this only worries about receiving socket messages
+		//LWW currently covers edits to existing items with entity ids.  Not newly created items.
 		let fromSocket = action.type.match(/[ADD_]+|[SOCKET_MESSAGE]+/);
 
 		let entityId = action.payload.entityId; //this could be a problem with current schema
 		//is this a socket based action?
 		switch (fromSocket) {
 			case 'ADD_':
-				//clear or add edits pending server confirmation
-				pendingEdits[entityId] ? delete pendingEdits[entityId] : (pendingEdits[entityId] = true);
+				if (!entityId) {
+					next(action);
+				} else {
+					pendingEdits[entityId] ? delete pendingEdits[entityId] : (pendingEdits[entityId] = true);
+				}
+			//clear or add edits pending server confirmation
 
 			case 'SOCKET_MESSAGE':
 				//Ignore incoming socket edits if they match any entries still pending server confirmation
