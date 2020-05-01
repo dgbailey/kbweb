5a1bfff100e96f1b3595a75695886fec5cddc466
dgbailey <bailey.dust@gmail.com>
2020-05-01 12:52:42 -0700

    starting to add test support for login

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/dom';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Login } from '../Login';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+const mockStore = configureMockStore();
+jest.mock('react-router-dom', () => ({
+	useHistory: () => ({
+		history: jest.fn()
+	})
+}));
+
+test('checks we are actually changing inputs', () => {
+	const testUsername = 'dustin';
+	const testPassword = 'test';
+	const store = mockStore({ loginStatus: true });
+
+	const { screen } = render(
+		<Provider store={store}>
+			<Login />
+		</Provider>
+	);
+	fireEvent.change(screen.getByLabelText(/username/i), { target: { value: 'dustin' } });
+	fireEvent.change(screen.getByLabelText(/password/i), { target: { value: 'test' } });
+
+	expect(screen.queryByText(testUsername)).toBe(testUsername);
+	expect(screen.queryByText(testUsername)).toBe(testUsername);
+});
