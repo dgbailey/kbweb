7b07e3936bb2504dce1de82de8eabfa318535aba
dgbailey <bailey.dust@gmail.com>
2020-05-08 09:56:07 -0700

    Clean implementation of configured Redux store
    -We were having issues with undefined variables originating from Redux.
    -Most likely case was from how were were composing enhancers (middleware + redux dev tools)
    -solution was to use the npm package redux-devtools-extension to compose

---
+++ src/utilities/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from 'redux';
+import { socketMiddleware } from '../../middleware/socketMiddleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from '../../reducers/index';
+export const configureStore = () => {
+	let middlewares = [ socketMiddleware() ];
+	let middlewareEnhancer = applyMiddleware(...middlewares);
+	const enhancers = [ middlewareEnhancer ];
+	const composedEnhancers = composeWithDevTools(...enhancers);
+	const store = createStore(rootReducer, composedEnhancers);
+	return store;
+};
